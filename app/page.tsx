a693d843f845ba1b9595b8f325bb3c7a461a438f
CalvinLinke <calvin.linke@freenet.de>
2026-06-13 14:05:58 +0200

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
+import Process from "../components/Process";
+import Services from "../components/Services";
+import Offerings from "../components/Offerings";
+import Trust from "../components/Trust";
+import Faq from "../components/Faq";
+import Cta from "../components/Cta";
+import Footer from "../components/Footer";
+import SiteEffects from "../components/SiteEffects";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Process />
+      <Services />
+      <Offerings />
+      <Trust />
+      <Faq />
+      <Cta />
+      <Footer />
+      <SiteEffects />
+    </>
+  );
+}
